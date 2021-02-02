66202e3ed3d369fc5c9d7c65b702b24a608f3124
Sylvie <sylvie.z.lu@gmail.com>
2021-02-02 13:30:23 +0100

    ➕:lock: updated server w/ cors & helmet

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
+const helmet = require('helmet');
 const morgan = require('morgan');
+
+const whitelist = [
+  process.env.REACT_APP_PROD_URL,
+  process.env.REACT_APP_DEV_URL,
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) != -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 const server = express();
 
@@ -12,7 +29,8 @@ const router = require('./routes');
 
 router.use(morgan('dev'));
 
-server.use('/api', router);
+server.use(helmet());
+server.use('/api', cors(corsOptions), router);
 server.use('*', (req, res, next) => {
   throw new NotFoundError('Not Found', 'Cannot find the requested resource');
 });
